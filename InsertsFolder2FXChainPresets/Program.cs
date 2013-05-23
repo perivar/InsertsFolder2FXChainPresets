ac91e0b5fcf3892b2115080a07ac9651c3a41a3a
Per Ivar Nerseth <perivar@nerseth.com>
2013-05-23 19:12:48 +0200

    Converted to preserve whitespace

--- InsertsFolder2FXChainPresets/Program.cs
+++ InsertsFolder2FXChainPresets/Program.cs
@@ -2,8 +2,10 @@
 using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.XPath;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 using CommonUtils;
 
@@ -11,7 +13,7 @@ namespace InsertsFolder2FXChainPresets
 {
 	class Program
 	{
-		public static string VERSION = "1.0.2";
+		public static string VERSION = "1.0.3";
 		
 		public static void Main(string[] args)
 		{
@@ -22,7 +24,7 @@ namespace InsertsFolder2FXChainPresets
 				string folderPresetsFilePath = args[0];
 				string outputDirectoryPath = args[1];
 				
-				if (InsertFolder2FXChainConvert(folderPresetsFilePath, outputDirectoryPath)) {
+				if (InsertFolder2FXChainConvert2(folderPresetsFilePath, outputDirectoryPath)) {
 					Console.WriteLine("Succesfully converted InsertsFolderPresets.pxml");
 				} else {
 					Console.WriteLine("Failed converted InsertsFolderPresets.pxml!");
@@ -129,11 +131,11 @@ namespace InsertsFolder2FXChainPresets
 						outputFileName = outputDirectoryPath + Path.DirectorySeparatorChar + outputFileName;
 						
 						// force explicit tag close for editController
-						XElement document = fxChainXmlDoc.GetXElement();						
+						XElement document = fxChainXmlDoc.GetXElement();
 						foreach (XElement childElement in
 						         from x in document.DescendantNodes().OfType<XElement>()
-						         where ( x.Attribute("name") != null 
-						                && x.Attribute("name").Value == "editController" 
+						         where ( x.Attribute("name") != null
+						                && x.Attribute("name").Value == "editController"
 						                && x.IsEmpty)
 						         select x)
 						{
@@ -166,6 +168,106 @@ namespace InsertsFolder2FXChainPresets
 							sw.Write(stringBuilder.ToString());
 						}
 						#endregion
+						
+						Console.WriteLine("Saving {0} ...", outputFileName);
+					}
+					return true;
+				} else {
+					Console.WriteLine("Output Directory not found! (\"{0}\")", outputDirectoryPath);
+				}
+			} else {
+				Console.WriteLine("Input File not found! (\"{0}\")", folderPresetsFilePath);
+			}
+			return false;
+		}
+		
+		public static bool InsertFolder2FXChainConvert2(string folderPresetsFilePath, string outputDirectoryPath) {
+
+			// How to split xml file into several files example
+			// http://social.msdn.microsoft.com/Forums/en-US/csharplanguage/thread/4d4ee78d-7798-4d0e-a267-d2bc104dc747
+			
+			// http://stackoverflow.com/questions/6289784/xpath-and-xpathselectelement
+			// The XPathSelectElement method can only be used to select elements, not attributes.
+			// For attributes, you need to use the more general XPathEvaluate method:
+			// var result = ((IEnumerable<object>)doc.XPathEvaluate("root/databases/db2/@name"))
+			//                          .OfType<XAttribute>()
+			//                          .Single()
+			//                          .Value;
+			
+			string presetName;
+			string outputFileName;
+			XDocument fxChainXmlDoc = null;
+			if (File.Exists(folderPresetsFilePath)) {
+				if (Directory.Exists(outputDirectoryPath)) {
+					XDocument folderPresetsXmlDoc = XDocument.Load(folderPresetsFilePath, LoadOptions.PreserveWhitespace);
+
+					// Split
+					IEnumerable<XElement> xnList = folderPresetsXmlDoc.XPathSelectElements("/Presets/rootObjects/root/list/item");
+					foreach (XElement xn in xnList)
+					{
+						var stringNode = xn.Elements("string").First(element => element.Attribute("name").Value == "Name");
+						var memberNode = xn.Elements("member").First(element => element.Attribute("name").Value == "Object");
+						
+						// Read preset name
+						presetName = stringNode.Attribute("value").Value;
+						
+						// Create the XmlDocument with default content
+						fxChainXmlDoc = new XDocument(new XDeclaration("1.0", "utf-8", null));
+						
+						// Use Linq XML (XElement) because they are easier to work with
+						XElement fxChainNode = new XElement("FxChainPreset");
+						fxChainXmlDoc.Add(fxChainNode);
+						
+						XElement mediaBayNode = new XElement("member",
+						                                     new XAttribute("name", "MediaBay")
+						                                    );
+						fxChainNode.Add(mediaBayNode);
+						
+						XElement listNode = new XElement("list",
+						                                 new XAttribute("name", "PAttr"),
+						                                 new XAttribute("type", "obj")
+						                                );
+						mediaBayNode.Add(listNode);
+
+						XElement objNode = new XElement("obj",
+						                                new XAttribute("class", "StMedia::PStringIDMediaAttribute"),
+						                                new XAttribute("ID", "4130621632")
+						                               );
+						listNode.Add(objNode);
+						
+						XElement id = new XElement("string",
+						                           new XAttribute("name", "ID"),
+						                           new XAttribute("value", "MediaType")
+						                          );
+						XElement type = new XElement("int",
+						                             new XAttribute("name", "Type"),
+						                             new XAttribute("value", "8")
+						                            );
+						XElement flags = new XElement("int",
+						                              new XAttribute("name", "Flags"),
+						                              new XAttribute("value", "2")
+						                             );
+						XElement str = new XElement("string",
+						                            new XAttribute("name", "String"),
+						                            new XAttribute("value", "FxChainPreset"),
+						                            new XAttribute("wide", "true")
+						                           );
+						
+						objNode.Add(id);
+						objNode.Add(type);
+						objNode.Add(flags);
+						objNode.Add(str);
+						
+						// Copy over the XML content from old XML file
+						XElement targetNode = new XElement(memberNode);
+						targetNode.Attribute("name").SetValue("Folder");           	// Change attribute name from Object to Folder
+						fxChainNode.Add(targetNode);
+						
+						outputFileName = StringUtils.MakeValidFileName(presetName) + ".fxchainpreset";
+						outputFileName = outputDirectoryPath + Path.DirectorySeparatorChar + outputFileName;
+						
+						//fxChainXmlDoc.Save(outputFileName, SaveOptions.DisableFormatting);
+						fxChainXmlDoc.Save(outputFileName);
 						
 						Console.WriteLine("Saving {0} ...", outputFileName);
 					}
